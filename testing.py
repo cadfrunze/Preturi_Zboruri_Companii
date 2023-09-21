55d85164b27ffb1c3c80d117daf815c12b436534
cadfrunze <marius1890@yahoo.com>
2023-09-21 22:08:09 +0300

    update

--- testing.py
+++ testing.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+import os
+import requests
 
-mesaj_actualizare_zi: str = datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M')
-print(mesaj_actualizare_zi)
+# mesaj_actualizare_zi: str = datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M')
+# print(mesaj_actualizare_zi)
 
-with open('./work_log.txt', 'w') as fisier:
-    fisier.writelines(mesaj_actualizare_zi)
+# with open('./work_log.txt', 'w') as fisier:
+#     fisier.writelines(mesaj_actualizare_zi)
+
+END_POINT_SHEETY = os.getenv('END_POINT_SHEETY')
+print(END_POINT_SHEETY)
