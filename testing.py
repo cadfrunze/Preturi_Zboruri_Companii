6980524eb5c994050dbf1a17f5ec7a05c5fd94a2
cadfrunze <marius1890@yahoo.com>
2023-09-25 12:26:08 +0300

    update

--- testing.py
+++ testing.py
@@ -1,3 +1,21 @@
-from datetime import datetime
-with open('./work_log.txt', mode='a') as fisier:
-    fisier.writelines('\n' + datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M'))
+# from data_base import END_POINT_SHEETY, AUTHORIZATION_SHEETY
+# import requests
+
+# # headers: dict = {
+# #     'Content-Type': 'application/json',
+# #     'Authorization': AUTHORIZATION_SHEETY
+# # }
+
+# # # cerere_SHEETY_get = requests.get(url=str(END_POINT_SHEETY), headers=headers)
+# # # print(cerere_SHEETY_get.text)
+# # editare: dict = {
+# #     'price': {
+# #         'city': 'Bucharest',
+# #         'lowestPrice': '30'
+# #     }
+# # }
+# # editare_SHEETY_editare = requests.put(url=f'{str(END_POINT_SHEETY)}/7', headers=headers, json=editare)
+# # print(editare_SHEETY_editare.text)
+
+
+
