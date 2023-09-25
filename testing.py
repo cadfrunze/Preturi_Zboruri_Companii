41a72b65c1fca2a4050da3b1228b83a0435e4169
cadfrunze <marius1890@yahoo.com>
2023-09-25 10:57:59 +0300

    update

--- testing.py
+++ testing.py
@@ -1,14 +1,3 @@
 from datetime import datetime
-import os
-import requests
-
-# mesaj_actualizare_zi: str = datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M')
-# print(mesaj_actualizare_zi)
-
-# with open('./work_log.txt', 'w') as fisier:
-#     fisier.writelines(mesaj_actualizare_zi)
-
-# AUTHORIZATION_SHEETY = 'Bearer fs'
-from data_base import END_POINT_SHEETY, AUTHORIZATION_SHEETY
-print(END_POINT_SHEETY)
-print(AUTHORIZATION_SHEETY)
+with open('./work_log.txt', mode='a') as fisier:
+    fisier.writelines('\n' + datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M'))
