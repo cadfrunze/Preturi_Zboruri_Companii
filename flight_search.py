e96b9ecf53a4b1720b1f8805a6d125dbf7ceb689
cadfrunze <marius1890@yahoo.com>
2023-09-25 20:55:02 +0300

    update

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,28 @@
+import requests
+from datetime import datetime
+from data_base import APP_KEY_TEQ
+API_TEQ_SEARCH = 'https://api.tequila.kiwi.com/v2/search'
+MY_CITY: str = 'Cluj-Napoca'
+MY_IATA: str = 'CLJ'
+# print(APP_KEY_TEQ)
+header_teq: dict = {
+    'apikey': str(APP_KEY_TEQ)
+
+}
+
+
+paramtri_search = {
+    'fly_from': MY_IATA,
+    'fly_to': 'NUE',
+    'date_from': f'{datetime.now().strftime("%d/%m/%Y")}',
+    'date_to': f'{datetime.now().strftime(f"29/%m/%Y")}'
+}
+
+cerere_test = requests.get(url=API_TEQ_SEARCH, headers=header_teq, params=paramtri_search)
+print(cerere_test.text)
+
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     pass
