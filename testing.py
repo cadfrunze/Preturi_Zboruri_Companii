ed465d9b97464cee910870e2c26fc8922665fd88
cadfrunze <marius1890@yahoo.com>
2023-09-29 13:11:13 +0300

    update

--- testing.py
+++ testing.py
@@ -23,14 +23,14 @@ header: dict = {
 
 cerere_SHEETY_get = requests.get(url=str(END_POINT_SHEETY), headers=header)
 print(cerere_SHEETY_get.text)
-editare: dict = {
-    'price': {
-        'city': 'Nuremberg',
-        'lowestPrice': '120'
-    }
-}
-editare_SHEETY_editare = requests.post(url=str(END_POINT_SHEETY), headers=header, json=editare)
-# editare_SHEETY_editare.raise_for_status()
-print(editare_SHEETY_editare.text)
-cerere = requests.get(url=str(END_POINT_SHEETY), headers=header)
-print(cerere.text)
+# editare: dict = {
+#     'price': {
+#         'city': 'Nuremberg',
+#         'lowestPrice': '120'
+#     }
+# }
+# editare_SHEETY_editare = requests.post(url=str(END_POINT_SHEETY), headers=header, json=editare)
+# # editare_SHEETY_editare.raise_for_status()
+# print(editare_SHEETY_editare.text)
+# cerere = requests.get(url=str(END_POINT_SHEETY), headers=header)
+# print(cerere.text)
---
+++ testing2.py
@@ -0,0 +1,40 @@
+from data_base import *
+import requests
+# exemplu: "prices": [
+    # {
+    #   "city": "Paris",
+    #   "iataCode": "",
+    #   "lowestPrice": 54,
+    #   "id": 2
+    # }
+print(END_POINT_SHEETY)
+header: dict = {
+    'Content-Type': 'application/json',
+    'Authorization': AUTHORIZATION_SHEETY
+}
+
+orase: dict = {
+    'Paris': 'PAR CDG LBG ORY',
+    'Berlin': 'BER SXF TXL THF',
+    'Tokyo': 'TYO HND NRT',
+    'Sydney': 'SYD',
+    'Istanbul': 'IST SAW',
+    'Bucharest': 'BUH OTP',
+    'New York': 'JFK LGA EWR NYC',
+    'San Francisco': 'SFO',
+    'Cape Town': 'CPT',
+    'Nuremberg': 'NUE'
+}
+
+# parcurgere: int = 2
+# for element in orase:
+#     editare_iata: dict = {
+#         'price': {
+#             'iataCode': orase[element]
+#         }
+#     }
+#     cerere_put = requests.put(url=f'{END_POINT_SHEETY}{parcurgere}', headers=header, json=editare_iata)
+#     parcurgere = parcurgere + 1
+#     print(cerere_put.text)
+coduri: str = orase['Berlin']
+print(coduri.split(' '))
