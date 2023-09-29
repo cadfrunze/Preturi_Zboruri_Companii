c048204341e70812779d8ae21646212f12af76e3
cadfrunze <marius1890@yahoo.com>
2023-09-29 11:56:46 +0300

    update

--- testing.py
+++ testing.py
@@ -1,23 +1,6 @@
-# from data_base import END_POINT_SHEETY, AUTHORIZATION_SHEETY
-# import requests
-# # header: dict = {
-# #     'Content-Type': 'application/json',
-# #     'Authorization': AUTHORIZATION_SHEETY
-# # }
+from data_base import END_POINT_SHEETY, AUTHORIZATION_SHEETY
+import requests
 
-# # cerere_SHEETY_get = requests.get(url=str(END_POINT_SHEETY), headers=headers)
-# # print(cerere_SHEETY_get.text)
-# # editare: dict = {
-# #     'price': {
-# #         'city': 'Nuremberg',
-# #         'lowestPrice': '120'
-# #     }
-# # }
-# # editare_SHEETY_editare = requests.post(url=str(END_POINT_SHEETY), headers=header, json=editare)
-# # # editare_SHEETY_editare.raise_for_status()
-# # print(editare_SHEETY_editare.text)
-# # cerere = requests.get(url=str(END_POINT_SHEETY), headers=header)
-# # print(cerere.text)
 # from datetime import datetime
 # range_zi: int = 2
 # ziua_calendar: int = datetime.now().day
@@ -32,3 +15,22 @@
 #         an_calendar: int = datetime.now().year + 1
 #     zi = datetime(year=an_calendar, month=luna_calendar, day=range_zi)
 # print(zi)
+
+header: dict = {
+    'Content-Type': 'application/json',
+    'Authorization': AUTHORIZATION_SHEETY
+}
+
+cerere_SHEETY_get = requests.get(url=str(END_POINT_SHEETY), headers=header)
+print(cerere_SHEETY_get.text)
+editare: dict = {
+    'price': {
+        'city': 'Nuremberg',
+        'lowestPrice': '120'
+    }
+}
+editare_SHEETY_editare = requests.post(url=str(END_POINT_SHEETY), headers=header, json=editare)
+# editare_SHEETY_editare.raise_for_status()
+print(editare_SHEETY_editare.text)
+cerere = requests.get(url=str(END_POINT_SHEETY), headers=header)
+print(cerere.text)
